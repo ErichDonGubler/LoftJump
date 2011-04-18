e17a0325c03a66fe0716de555b654982dbd1478c
Erich Gubler <k0ryu0bihir0@gmail.com>
2011-04-17 20:39:41 -0600

    Tons of stuff! Added file config with multiworld, check and reload commands to go with it, and updated some permissions.

--- src/com/KoryuObihiro/bukkit/loftjump/LoftJump.java
+++ src/com/KoryuObihiro/bukkit/loftjump/LoftJump.java
@@ -1,10 +1,10 @@
 package com.KoryuObihiro.bukkit.loftjump;
 
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -16,6 +16,7 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import com.nijiko.permissions.PermissionHandler;
 import com.nijikokun.bukkit.Permissions.Permissions;
+
 import java.util.logging.Logger;
 
 /**
@@ -29,17 +30,12 @@ public class LoftJump extends JavaPlugin{
 	private final LoftJumpEntityListener blockListener = new LoftJumpEntityListener(this);
 	public final HashMap<Player, ArrayList<Block>> LoftJumpers = new HashMap<Player, ArrayList<Block>>();
 	public final HashMap<Player, ArrayList<Block>> freeMen = new HashMap<Player, ArrayList<Block>>();
+	public HashMap<World, LoftJumpConfiguration> configs = new HashMap<World, LoftJumpConfiguration>();
 	public static Logger log = Logger.getLogger("Minecraft");
 	public static PermissionHandler Permissions = null;
 	
+	//TODO There's a reason to question the use of the HashMap for players right now, with Permissions used - IDK
 	//DEFAULT SETTINGS
-	public boolean settings_use_onByDefault = true;
-	public boolean settings_free_onByDefault = false;
-	public Material settings_ConsumeMaterial = Material.FEATHER;
-	public Material settings_HoldMaterial = Material.FEATHER;
-	public int settings_costPerDamage = 1;
-	
-//SETTINGS TODO add changing of settings ingame, perhaps persistent with file read/write
 	
 ////////////////////////// INITIALIZATION ///////////////////////////////
 	@Override
@@ -64,8 +60,11 @@ public class LoftJump extends JavaPlugin{
 		getServer().getPluginManager().registerEvent(Event.Type.PLAYER_KICK, playerListener, Event.Priority.Normal, this);
 		getServer().getPluginManager().registerEvent(Event.Type.PLAYER_QUIT, playerListener, Event.Priority.Normal, this);
 		
-		//placeholder for loadSettings, currently does nothing TODO
-		loadSettings();
+		for(World world : getServer().getWorlds())
+		{
+			configs.put(world, new LoftJumpConfiguration(world, this));
+			configs.get(world).loadSettings(this.getConfiguration());
+		}
 	}
 	
 	@Override
@@ -75,11 +74,6 @@ public class LoftJump extends JavaPlugin{
 		log.info("["+getDescription().getName()+"] disabled.");	
 	}
 	
-	public void loadSettings()
-	{
-		//TODO
-		return;
-	}
 	
 ///////////////////// COMMAND HANDLING //////////////////////////////
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
@@ -100,20 +94,13 @@ public class LoftJump extends JavaPlugin{
 				//handle external commands like console, IRC, etc.
 				if(player == null)
 				{
-						log.info("[LoftJump]Commands:\n" +
-								"loftjump (alias /lj) - brings up this help message\n" +
-								"loftjump toggle (player) (alias /lj t) - toggle LoftJump on a player\n" +
-								"loftjump free (player) (alias /lj f) - toggle item consume on a player");
-						return true;
+					sendUsage(player);
+					return true;
 				}
 				
 				//handle player usage
 				sendUsage(player);
-				if(hasPermission(player, "loftjump.check")) 
-				{
-					player.sendMessage(ChatColor.DARK_RED + "Material cost per 1/2-heart damage: " + settings_costPerDamage);
-				}
-					return true;
+				return true;
 			}
 			else if(args.length >= 0)
 			{
@@ -144,23 +131,29 @@ public class LoftJump extends JavaPlugin{
 						{
 							if(player == null)
 								return toggleFromConsole(playerMatch);
-							else if(hasPermission(player, "loftjump.toggleothers"))
+							else if(hasPermission(player, "loftjump.toggle.others"))
 								toggleUsage(playerMatch, true, player);
-							else player.sendMessage(ChatColor.RED + "[LoftJump]You don't have permission to toggle others.");
+							else player.sendMessage(ChatColor.RED + "[LoftJump] You don't have permission to toggle others.");
 							return true;
 						}
 						else
 						{
 							if(player == null) log.info("Error: Couldn't find matching player substring.");
-							else player.sendMessage(ChatColor.RED + "[LoftJump]Couldn't find matching player name.");
+							else player.sendMessage(ChatColor.RED + "[LoftJump] Couldn't find matching player name.");
 							return true;
 						}
 					}
 				}
 				else if(args[0].equalsIgnoreCase("free") || args[0].equalsIgnoreCase("f"))
 				{
+					
 					if(args.length == 1)
 					{
+						if(player == null)
+						{
+								log.info("Error: player not specified.");
+								return true;
+						}
 						return toggleFreedom(player);
 					}
 					else if(args.length == 2)
@@ -178,30 +171,146 @@ public class LoftJump extends JavaPlugin{
 						{
 							if(player == null)
 								return toggleFreeFromConsole(playerMatch);
-							else if(hasPermission(player, "loftjump.freeothers"))
+							else if(hasPermission(player, "loftjump.free.others"))
 								toggleFreedom(playerMatch, true, player);
-							else player.sendMessage(ChatColor.RED + "[LoftJump]You don't have permission to toggle others.");
+							else player.sendMessage(ChatColor.RED + "[LoftJump] You don't have permission to toggle others.");
 							return true;
 						}
 						else
 						{
 							if(player == null) log.info("Error: Couldn't find matching player substring.");
-							else player.sendMessage(ChatColor.RED + "[LoftJump]Couldn't find matching player name.");
+							else player.sendMessage(ChatColor.RED + "[LoftJump] Couldn't find matching player name.");
 							return true;
 						}
 					}
 				}
+				else if(args[0].equalsIgnoreCase("check") || args[0].equalsIgnoreCase("c"))
+				{
+					if(args.length == 1)
+					{
+						if(player == null)
+						{
+								log.info("Error: world not specified.");
+								return true;
+						}
+						else if(hasPermission(player, "loftjump.check")) 
+						{
+							sendWorldConfig(player, player.getWorld());
+						}
+						else player.sendMessage(ChatColor.RED + "[LoftJump] You don't have access to that command.");
+						return true;
+					}
+					else if(args.length == 2)
+					{
+						World worldMatch = null;
+						for(World temp : getServer().getWorlds())
+							for(int i = 0; i < (temp.getName().length() - args[1].length() - 1); i++)
+								if(args[1].equalsIgnoreCase(temp.getName().substring(i, i + args[1].length())))
+								{
+									worldMatch = temp;
+									break;
+								}
+						
+						if(worldMatch != null)
+						{
+							if(player == null) return sendWorldConfig(player, worldMatch);
+							else if(hasPermission(player, "loftjump.check.others"))
+								sendWorldConfig(player, worldMatch);
+							else player.sendMessage(ChatColor.RED + "[LoftJump] You don't have permission to check other worlds.");
+							return true;
+						}
+						else
+						{
+							if(player == null) log.info("Error: Couldn't find matching world substring.");
+							else player.sendMessage(ChatColor.RED + "[LoftJump] Couldn't find matching world name.");
+							return true;
+						}
+					}
+				}
+				else if(args[0].equalsIgnoreCase("reload"))
+				{
+					if(player == null) return reloadConfig(player);
+					else if(hasPermission(player, "loftjump.reload"))
+						return reloadConfig(player);
+					player.sendMessage(ChatColor.RED + "[LoftJump] You don't have access to that command.");
+				}
 			}
 		}
 		return sendUsage(player);
+	}	
+
+	private boolean sendWorldConfig(Player player, World world) 
+	{
+		LoftJumpConfiguration thisConfig = configs.get(world);
+		if(player != null)
+		{
+			player.sendMessage(ChatColor.GREEN + "LoftJump status:" + 
+					(LoftJumpers.containsKey(player)?("on"):(ChatColor.DARK_RED + "off")) +
+					(freeMen.containsKey(player)?("(item consumption off)"):("")));
+		    player.sendMessage(ChatColor.YELLOW + "LoftJump settings for world " + ChatColor.DARK_PURPLE + world.getName());
+		 
+		 //holdMaterial   
+		    if(!thisConfig.get_use_onByDefault())
+		    	player.sendMessage(ChatColor.YELLOW + "holdMaterial_use: " 
+		    		+ ChatColor.DARK_RED + "false");
+		    else player.sendMessage(ChatColor.YELLOW + "holdMaterial: " 
+		    		+ ChatColor.AQUA + thisConfig.get_HoldMaterial().name());
+		
+		//consumeMaterial    
+		    player.sendMessage(ChatColor.YELLOW + "consumeMaterial: " 
+					+ ChatColor.AQUA + thisConfig.get_ConsumeMaterial().name());
+		
+		//cost
+		    player.sendMessage(ChatColor.YELLOW + "Cost/half heart: " 
+					+ ChatColor.AQUA + thisConfig.get_Cost());
+		    
+		//use_onByDefault   
+		    player.sendMessage(((thisConfig.get_use_onByDefault())
+		    	?(ChatColor.YELLOW + "use_onByDefault: " + ChatColor.GREEN + thisConfig.get_use_onByDefault())
+		    	:(ChatColor.YELLOW + "use_onByDefault: " + ChatColor.DARK_RED + thisConfig.get_use_onByDefault())));
+		    
+		//free_onByDefault
+		    player.sendMessage((thisConfig.get_free_onByDefault())
+		    	?(ChatColor.YELLOW + "free_OnByDefault: " + ChatColor.GREEN + thisConfig.get_free_onByDefault())
+		    	:(ChatColor.YELLOW + "free_onByDefault: " + ChatColor.DARK_RED + thisConfig.get_free_onByDefault()));
+		}
+		else
+		{
+			log.info("[LoftJump] Settings for world " + world.getName() + ":" +
+					((thisConfig.get_HoldMaterial_use())
+							?("\nholdMaterial: " + thisConfig.get_HoldMaterial())
+							:("\nholdMaterial_use: " + thisConfig.get_HoldMaterial_use())) +
+					"\nconsumeMaterial: " + thisConfig.get_ConsumeMaterial() +
+					"\ncost: " + thisConfig.get_Cost() +
+					"\nuse_onByDefault: " + thisConfig.get_use_onByDefault() +
+					"\nfree_onByDefault: " + thisConfig.get_free_onByDefault());
+		}
+	    
+	    return true;
 	}
-	
+
 	private boolean sendUsage(Player player) 
 	{
-		player.sendMessage(ChatColor.LIGHT_PURPLE + "LoftJump commands: ");
-		player.sendMessage(ChatColor.LIGHT_PURPLE + "/loftjump (alias /lj) - brings up this help message");
-		if(hasPermission(player, "loftjump.toggle")) player.sendMessage(ChatColor.LIGHT_PURPLE + "/loftjump toggle [player] (alias /lj t) - toggle LoftJump");
-		if(hasPermission(player, "loftjump.free")) player.sendMessage(ChatColor.LIGHT_PURPLE + "/loftjump free [player] (alias /lj f) - toggle item consume");
+		if(player != null)
+		{
+			player.sendMessage(ChatColor.LIGHT_PURPLE + "LoftJump commands: ");
+			player.sendMessage(ChatColor.LIGHT_PURPLE + "/loftjump (alias /lj) - brings up this help message");
+			if(hasPermission(player, "loftjump.toggle")) player.sendMessage(ChatColor.LIGHT_PURPLE + "/loftjump toggle [player] (alias /lj t) - toggle LoftJump");
+			if(hasPermission(player, "loftjump.free")) player.sendMessage(ChatColor.LIGHT_PURPLE + "/loftjump free [player] (alias /lj f) - toggle item consume");
+			if(hasPermission(player, "loftjump.check")) player.sendMessage(ChatColor.LIGHT_PURPLE + "/loftjump check [world] (alias /lf c) - check world config");
+			if(hasPermission(player, "loftjump.reload")) player.sendMessage(ChatColor.LIGHT_PURPLE + "/loftjump reload - reload file configuration");
+		}
+		else
+		{
+			log.info("[LoftJump] Commands:\n" +
+					"loftjump (alias lj) - brings up this help message\n" +
+					"loftjump toggle (player) (alias lj t) - toggle LoftJump on a player\n" +
+					"loftjump free (player) (alias lj f) - toggle item consume on a player\n" +
+					"loftjump check (world) (alias lf c) - check config for a world\n" +
+					"loftjump reload - reload file configuration");
+		}
+		
+		
 		return true;
 	}
 	
@@ -229,7 +338,7 @@ public class LoftJump extends JavaPlugin{
 						+ "LoftJump for " + player.getName() + " enabled.");
 			}
 		  }
-		  else player.sendMessage(ChatColor.RED + "[LoftJump]You don't have permission to do that.");
+		  else player.sendMessage(ChatColor.RED + "[LoftJump] You don't have permission to do that.");
 		  return true;
 	  }
 	  
@@ -257,14 +366,13 @@ public class LoftJump extends JavaPlugin{
 							+ "LoftJump item consumption for " + player.getName() + " disabled.");
 				}
 			  }
-			  else player.sendMessage(ChatColor.RED + "[LoftJump]You don't have permission to do that.");
+			  else player.sendMessage(ChatColor.RED + "[LoftJump] You don't have permission to do that.");
 		  }
 		  else
 		  {
-			  if(sender != null)
-				  sender.sendMessage(ChatColor.RED + "Error: That person does not have Loftjump enabled.");
-			  else
-				  player.sendMessage(ChatColor.RED + "Error: You do not have LoftJump enabled.");
+			  sender.sendMessage(ChatColor.RED + ((sender != null)
+					?"Error: That person does not have Loftjump enabled."
+					:"Error: You do not have LoftJump enabled."));
 		  }
 		  
 		  return true;
@@ -293,13 +401,13 @@ public class LoftJump extends JavaPlugin{
 			{
 				this.freeMen.remove(player);
 				player.sendMessage(ChatColor.GREEN + "LoftJump item consumption enabled.");
-				log.info("[LoftJump]Item consumption for " + player.getName() + " enabled.");
+				log.info("[LoftJump] Item consumption for " + player.getName() + " enabled.");
 			} 
 			else 
 			{
 				this.freeMen.put(player, null);
 				player.sendMessage(ChatColor.GREEN + "LoftJump item consumption disabled.");
-				log.info("[LoftJump]Item consumption for " + player.getName() + " disabled.");
+				log.info("[LoftJump] Item consumption for " + player.getName() + " disabled.");
 			}
 		  return true;
 	  }
@@ -308,39 +416,42 @@ public class LoftJump extends JavaPlugin{
 	  
 	public void tryLoftJump(Player player, EntityDamageEvent event)
 	{
+		LoftJumpConfiguration thisConfig = configs.get(player.getWorld());
+		int cost = thisConfig.get_Cost();
+		Material toConsume = thisConfig.get_ConsumeMaterial();
+		Material holdMe = thisConfig.get_HoldMaterial();
+		
 		if(!player_Enabled(player) || !hasPermission(player, "loftjump.use")) return;
 		//get information about the player's held item, check against the settings for that world
 		ItemStack presumablyFeathers = player.getItemInHand();
 		int damage = event.getDamage();
-		if(presumablyFeathers.getType().equals(settings_HoldMaterial))
+		if(!thisConfig.get_HoldMaterial_use() || presumablyFeathers.getType().equals(holdMe))
 		{
 			//count the number of consumable items in the players inventory
 			int totalConsumables = 0;
 			for (ItemStack item : player.getInventory().getContents())
 			{
 				if (item == null) continue;//have to do this for recent builds of CB
-				else if (settings_ConsumeMaterial.equals(item.getType()))
+				else if(toConsume.equals(item.getType()))
 						totalConsumables += item.getAmount();
 			}
 			
 			//do some damage calculation
-			int netDamage = damage * settings_costPerDamage - totalConsumables;
+			int netDamage = damage * cost - totalConsumables;			
 			if(netDamage >= 0 && !player_isFree(player))//unless they have the right perm
-			{
-				//For technical reasons, we still want the fall event to continue after the damage is handled.
-				event.setDamage(netDamage);
-				
-				//Since we incurred damage, that can only mean
-				// we're out of feathers. :(
-				player.getInventory().removeItem(new ItemStack(settings_ConsumeMaterial, damage));
-			}
-			else 
-			{
-				event.setDamage(0); //obviously
-				//subtract the amount of feathers necessary
-				if(!player_isFree(player)) 
-					player.getInventory().removeItem(new ItemStack(settings_ConsumeMaterial, damage));
-			}
+				event.setDamage(netDamage); //For technical reasons, we still want the fall event to continue after the damage is handled.
+			else event.setDamage(0); //obviously
+			
+			//subtract the necessary feathers...unless they don't need to. :P
+			if(!player_isFree(player)) 
+				player.getInventory().removeItem(new ItemStack(toConsume, damage * cost));
+			
+			/*
+			player.sendMessage("Damage incurred: " + damage);
+			player.sendMessage("Damage cost: " + settings_costPerDamage);
+			player.sendMessage("Feathers you have: " + totalConsumables);
+			player.sendMessage("Net damage: " + netDamage);
+			*/ //Some debugging stuff. :D
 		}
 		return;
 	}
@@ -358,11 +469,24 @@ public class LoftJump extends JavaPlugin{
 		return player.isOp();
 	}
 
-//player_Enabled function
-//returns a bool based on whether the inputted player is on the LoftJumpers hashmap or not
-public boolean player_Enabled(Player player) {return this.LoftJumpers.containsKey(player);}
-public boolean player_isFree(Player player) {return this.freeMen.containsKey(player);}
+	//player_Enabled function
+	//returns a bool based on whether the inputted player is on the LoftJumpers hashmap or not
+	public boolean player_Enabled(Player player) {return this.LoftJumpers.containsKey(player);}
+	public boolean player_isFree(Player player) {return this.freeMen.containsKey(player);}
 
 	  
+	private boolean reloadConfig(Player player) 
+	{
+		configs.clear();
+		for(World world : getServer().getWorlds())
+		{
+			configs.put(world, new LoftJumpConfiguration(world, this));
+			configs.get(world).loadSettings(this.getConfiguration());
+		}
+		if(player == null) log.info("[LoftJump] World configuration reloaded.");
+		else player.sendMessage(ChatColor.GREEN + "[LoftJump] World configuration reloaded.");
+		
+		return true;
+	}
 	  
 }
--- src/com/KoryuObihiro/bukkit/loftjump/LoftJumpPlayerListener.java
+++ src/com/KoryuObihiro/bukkit/loftjump/LoftJumpPlayerListener.java
@@ -1,5 +1,7 @@
 package com.KoryuObihiro.bukkit.loftjump;
 
+import org.bukkit.World;
+
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerListener;
@@ -11,19 +13,18 @@ public class LoftJumpPlayerListener extends PlayerListener
 //Members
 	private LoftJump plugin;
 	//Constructors	
-	public LoftJumpPlayerListener(LoftJump plugin) 
-	{
-		this.plugin = plugin;
-	}
+	public LoftJumpPlayerListener(LoftJump plugin) {this.plugin = plugin;}
 	
 //Functions
 	@Override
 	public void onPlayerJoin(PlayerJoinEvent event)
 	{
-		if(plugin.settings_use_onByDefault && !plugin.player_Enabled(event.getPlayer()) 
+		//enable LJ and LJ-free according to settings
+		World world = event.getPlayer().getWorld();
+		if(plugin.configs.get(world).get_use_onByDefault() && !plugin.player_Enabled(event.getPlayer()) 
 				&& LoftJump.hasPermission(event.getPlayer(), "loftjump.use"))
 			plugin.toggleUsage(event.getPlayer(), false);
-		if(plugin.settings_free_onByDefault && !plugin.player_isFree(event.getPlayer()) 
+		if(plugin.configs.get(world).get_free_onByDefault() && !plugin.player_isFree(event.getPlayer()) 
 				&& LoftJump.hasPermission(event.getPlayer(), "loftjump.free"))
 			plugin.toggleFreedom(event.getPlayer(), false);
 	}
@@ -31,19 +32,15 @@ public class LoftJumpPlayerListener extends PlayerListener
 	@Override
 	public void onPlayerQuit(PlayerQuitEvent event)
 	{
-		if (plugin.player_Enabled(event.getPlayer()))
-			plugin.toggleUsage(event.getPlayer(), false);
-		if(plugin.player_isFree(event.getPlayer()))
-			plugin.toggleFreedom(event.getPlayer(), false);
+		if (plugin.player_Enabled(event.getPlayer())) plugin.toggleUsage(event.getPlayer(), false);
+		if(plugin.player_isFree(event.getPlayer())) plugin.toggleFreedom(event.getPlayer(), false);
 	}
 	
 	@Override
 	public void onPlayerKick(PlayerKickEvent event)
 	{
-		if (plugin.player_Enabled(event.getPlayer()))
-			plugin.toggleUsage(event.getPlayer(), false);
-		if(plugin.player_isFree(event.getPlayer()))
-			plugin.toggleFreedom(event.getPlayer(), false);
+		if (plugin.player_Enabled(event.getPlayer())) plugin.toggleUsage(event.getPlayer(), false);
+		if(plugin.player_isFree(event.getPlayer())) plugin.toggleFreedom(event.getPlayer(), false);
 	}
 				 
 }
